772596ecad5e242facd98f20cc6545e46e2b6d9f
NickmDrummer <nickmdrummer@gmail.com>
2025-02-25 02:14:38 -0300

    new brew script

---
+++ scripts/brewpackages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Instaliing yazi"
+brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide imagemagick
+
+echo "Installing starship prompt"
+curl -sS https://starship.rs/install.sh | sh
+
+echo "Installing Maple Mono Font"
+brew install --cask font-maple-nf
