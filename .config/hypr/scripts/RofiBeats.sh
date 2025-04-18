a6b8d20891d448f3ecbd82ba1b1f31358bd03fd5
NickmDrummer <nickmdrummer@gmail.com>
2025-04-18 17:57:15 -0300

    fix: stations and playlists

--- .config/hypr/scripts/RofiBeats.sh
+++ .config/hypr/scripts/RofiBeats.sh
@@ -10,23 +10,15 @@ rofi_theme_1="$HOME/.config/rofi/config-rofi-Beats-menu.rasi"
 
 # Online Stations. Edit as required
 declare -A online_music=(
-  ["FM - Easy Rock 96.3 📻🎶"]="https://radio-stations-philippines.com/easy-rock"
   ["FM - Easy Rock - Baguio 91.9 📻🎶"]="https://radio-stations-philippines.com/easy-rock-baguio"
   ["FM - Love Radio 90.7 📻🎶"]="https://radio-stations-philippines.com/love"
-  ["FM - WRock - CEBU 96.3 📻🎶"]="https://onlineradio.ph/126-96-3-wrock.html"
-  ["FM - Fresh Philippines 📻🎶"]="https://onlineradio.ph/553-fresh-fm.html"
   ["Radio - Lofi Girl 🎧🎶"]="https://play.streamafrica.net/lofiradio"
   ["Radio - Chillhop 🎧🎶"]="http://stream.zeno.fm/fyn8eh3h5f8uv"
   ["Radio - Ibiza Global 🎧🎶"]="https://filtermusic.net/ibiza-global"
-  ["Radio - Metal Music 🎧🎶"]="https://tunein.com/radio/mETaLmuSicRaDio-s119867/"
-  ["YT - Wish 107.5 YT Pinoy HipHop 📻🎶"]="https://youtube.com/playlist?list=PLkrzfEDjeYJnmgMYwCKid4XIFqUKBVWEs&si=vahW_noh4UDJ5d37"
-  ["YT - Youtube Top 100 Songs Global 📹🎶"]="https://youtube.com/playlist?list=PL4fGSI1pDJn6puJdseH2Rt9sMvt9E2M4i&si=5jsyfqcoUXBCSLeu"
-  ["YT - Wish 107.5 YT Wishclusives 📹🎶"]="https://youtube.com/playlist?list=PLkrzfEDjeYJn5B22H9HOWP3Kxxs-DkPSM&si=d_Ld2OKhGvpH48WO"
   ["YT - Relaxing Piano Music 🎹🎶"]="https://youtu.be/6H7hXzjFoVU?si=nZTPREC9lnK1JJUG"
-  ["YT - Youtube Remix 📹🎶"]="https://youtube.com/playlist?list=PLeqTkIUlrZXlSNn3tcXAa-zbo95j0iN-0"
-  ["YT - Korean Drama OST 📹🎶"]="https://youtube.com/playlist?list=PLUge_o9AIFp4HuA-A3e3ZqENh63LuRRlQ"
   ["YT - lofi hip hop radio beats 📹🎶"]="https://www.youtube.com/live/jfKfPfyJRdk?si=PnJIA9ErQIAw6-qd"
   ["YT - Relaxing Piano Jazz Music 🎹🎶"]="https://youtu.be/85UEqRat6E4?si=jXQL1Yp2VP_G6NSn"
+  ["Primeagen Bangers 💻"]="https://www.youtube.com/playlist?list=PLm323Lc7iSW9oSIDihesMJXmMNfh8U59k"
 )
 
 # Populate local_music array with files from music directory and subdirectories
@@ -60,7 +52,7 @@ play_local_music() {
     if [ "${filenames[$i]}" = "$choice" ]; then
 
       notification "$choice"
-      mpv --playlist-start="$i" --volume=70 --volume-max=100 --loop-playlist --vid=no "${local_music[@]}"
+      mpv --playlist-start="$i" --loop-playlist --vid=no "${local_music[@]}"
 
       break
     fi
