2b074dc80dae6de542f1271d5fcea57f8480d610
NickmDrummer <nickmdrummer@gmail.com>
2025-04-22 00:09:10 -0300

    fix: changing to english

--- .config/waybar/script/toggle-port.sh
+++ .config/waybar/script/toggle-port.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# Nombre del sink
 SINK_NAME="alsa_output.pci-0000_0c_00.6.analog-stereo"
 
-# Puertos disponibles
 PORT_HEADPHONES="analog-output-headphones"
 PORT_LINEOUT="analog-output-lineout"
-PORT_HEADPHONES_NAME="PARLANTES"
-PORT_LINEOUT_NAME="AURICULARES"
+PORT_HEADPHONES_NAME="SPEAKERS"
+PORT_LINEOUT_NAME="HEADPHONES"
 
-# Archivo para almacenar el puerto actual
+# File to store the current port
 STATE_FILE="$HOME/.cache/current_audio_port"
 
-# Leer el puerto actual del archivo de estado, o usar auriculares por defecto
 if [ -f "$STATE_FILE" ]; then
   CURRENT_PORT=$(cat "$STATE_FILE")
 else
   CURRENT_PORT="$PORT_HEADPHONES"
 fi
 
-# Debugging: Mostrar el puerto actual asumido
-echo "Puerto actual asumido (de state file): $CURRENT_PORT" >&2
+# Debugging:
+echo "Current Port (from state file): $CURRENT_PORT" >&2
 
-# Determinar el próximo puerto
+# Determine the next port to use
 if [ "$CURRENT_PORT" = "$PORT_HEADPHONES" ]; then
   NEXT_PORT="$PORT_LINEOUT"
   NEXT_PORT_NAME="$PORT_LINEOUT_NAME"
@@ -31,19 +28,19 @@ else
   NEXT_PORT_NAME="$PORT_HEADPHONES_NAME"
 fi
 
-# Debugging: Mostrar el puerto al que intentamos cambiar
-echo "Intentando cambiar a: $NEXT_PORT" >&2
+# Debugging: show the port to change
+echo "Trying to change to $NEXT_PORT" >&2
 
-# Cambiar al próximo puerto
+# Change to the next port
 pactl set-sink-port "$SINK_NAME" "$NEXT_PORT"
 
-# Debugging: Mostrar la salida completa de pactl para el sink
-echo "Salida de pactl list sinks para el sink:" >&2
+# Debugging: Show the complete output from pactl
+echo "pactl list sinks:" >&2
 pactl list sinks | grep -A 100 "Name: $SINK_NAME" | grep "Active Port" >&2
 
-# Guardar el nuevo puerto en el archivo de estado
+# Save the new port in the state's file
 echo "$NEXT_PORT" >"$STATE_FILE"
 
-# Mostrar notificación
-notify-send "Salida de audio cambiada" "$NEXT_PORT_NAME" -t 2000
+# Notify the user
+notify-send "Audio Output Changed" "$NEXT_PORT_NAME" -t 2000
 echo "Cambiado exitosamente a: $NEXT_PORT_NAME" >&2
