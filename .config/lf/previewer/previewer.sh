ad6f187697f543bde05930f861d0964aa7bac0dc
NickM <nickmdrummer@gmail.com>
2025-05-06 23:29:05 -0300

    fix(lf): fix scripts to work with ghostty using kitty and pistol

--- .config/lf/previewer/previewer.sh
+++ .config/lf/previewer/previewer.sh
@@ -1,24 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/sh
+draw() {
+  kitten icat --stdin no --transfer-mode memory --place "${w}x${h}@${x}x${y}" "$1" </dev/null >/dev/tty
+  exit 1
+}
 
-file=$1
-w=$2
-h=$3
-x=$4
-y=$5
+file="$1"
+w="$2"
+h="$3"
+x="$4"
+y="$5"
 
-if [[ "$( file -Lb --mime-type "$file")" =~ ^image ]]; then
-    kitty +kitten icat --silent --stdin no --transfer-mode file --place "${w}x${h}@${x}x${y}" "$file" < /dev/null > /dev/tty
-    exit 1
-fi
-
-case "$1" in
-    *.png|*.jpg|*.jpeg|*gif) icat -w 100 "$1";;
-    *.pdf) pdftotext "$1" -;;
-    *.zip) zipinfo "$1";;
-    *.tar.gz) tar -ztvf "$1";;
-    *.tar.bz2) tar -jtvf "$1";;
-    *.tar) tar -tvf "$1";;
-    *) bat --color=always --style=plain --pager=never "$1" "$@";;
+case "$(file -Lb --mime-type "$file")" in
+image/*)
+  draw "$file"
+  ;;
+video/*)
+  # vidthumb is from here:
+  # https://raw.githubusercontent.com/duganchen/kitty-pistol-previewer/main/vidthumb
+  draw "$(vidthumb "$file")"
+  ;;
 esac
 
 pistol "$file"
